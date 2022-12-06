adeae5c07fa5e5bc2c374276ab7e5c55535e3171
Adam Caine <adam.caine10@gmail.com>
2022-12-06 13:30:27 +0000

    Add comments to tuningtrouble.cpp

--- Day6/tuningtrouble.cpp
+++ Day6/tuningtrouble.cpp
@@ -1,40 +1,47 @@
 #include "filereader.h"
 #include <iostream>
 
-bool hasNoRepeatedCharacters(std::string line);
+bool uniqueChars(std::string substr);
+
+const uint8_t CHARS_IN_PACKET = 4;
+const uint8_t CHARS_IN_MESSAGE = 14;
 
 int main () {
 
   // Create a file reader to read the file in from the input
   std::string line;
-
   FileReader fr = FileReader("input.txt");
 
+  // Variables to store the positions of the start-of-packet and start-of-message indexes
   uint32_t startOfPacket = -1;
   uint32_t startOfMessage = -1;
 
+  // If we can't read a line from the file then fail the program
   if (!fr.readLine(&line)) {
     return EXIT_FAILURE;
   }
 
-  std::cout << "Reading input: " << line << std::endl;
-
   // Loop through the line and figure out where the start of the input thingy is
 
+  // Variable to keep track of where we stopped with the start of packet loop
+  // We are not going to find 14 unique characters if we haven't found 4 in the string so far
+  // So the second loop may continue from this point.
   int finalMarker = 0;
 
-  for (int marker = 0; marker < line.length() - 4; marker++) {
+  // Iterate through all substrings of length 4 until we find one with all unique characters
+  for (int marker = 0; marker < line.length() - CHARS_IN_PACKET; marker++) {
     // Calculate the substring of the last 4 characters before the marker
-    if (hasNoRepeatedCharacters(line.substr(marker, 4))) {
-      startOfPacket = marker + 4;
+    if (uniqueChars(line.substr(marker, CHARS_IN_PACKET))) {
+      startOfPacket = marker + CHARS_IN_PACKET;
       finalMarker = marker;
       break;
     }
   }
 
-  for (int marker = finalMarker; marker < line.length() - 14; marker++) {
-    if (hasNoRepeatedCharacters(line.substr(marker, 14))) {
-      startOfMessage = marker + 14;
+  // Iterate through all substrings of length 14 until we find one with all unique characters
+  for (int marker = finalMarker; marker < line.length() - CHARS_IN_MESSAGE; marker++) {
+    if (uniqueChars(line.substr(marker, CHARS_IN_MESSAGE))) {
+      startOfMessage = marker + CHARS_IN_MESSAGE;
       finalMarker = marker;
       break;
     }
@@ -46,12 +53,13 @@ int main () {
   return EXIT_SUCCESS;
 }
 
-bool hasNoRepeatedCharacters(std::string line) {
-  // Loop through all of the characters and compare it to the rest
-  for (int first = 0; first < line.length() - 1; first++) {
-    for (int second = first + 1; second < line.length(); second++) {
-
-      if (line.at(first) == line.at(second)) return false;
+bool uniqueChars(std::string substr) {
+  // Loop through all of the characters and compare it to the rest of characters in the substring
+  for (int first = 0; first < substr.length() - 1; first++) {
+    for (int second = first + 1; second < substr.length(); second++) {
+      
+      // If we find a repeated character then just exit out of the function
+      if (substr.at(first) == substr.at(second)) return false;
 
     }
   }
